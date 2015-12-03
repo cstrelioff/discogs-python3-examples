f26583d76ac83cdcb9d3145c1d3157ee841e1040
Christopher C Strelioff <chris.strelioff@gmail.com>
2015-12-03 11:14:30 -0800

    update config-example.py; fix typos

--- config-example.py
+++ config-example.py
@@ -8,14 +8,17 @@
 
 """
 config.py
-=========
 
 An example of the config.py file -- change this filename to config.py for
-using with exmple scripts. The gitignore file has been setup to ignore the 
-config.py fiel, keeping tokens safe.
+using with the example scripts. The gitignore file has been setup to ignore
+the config.py file, keeping your tokens out of public repos. Obtain your
+token(s) here:
 
-Keep all secretet tokens here -- set these up at this line:
-
-    https://www.discogs.com/settings/developers
+https://www.discogs.com/settings/developers
 """
+# if you only want to access your own data
 user_token = 'your-user-token-here'
+
+# if you want to access data from other user accounts after authentication
+consumer_key = 'your-consumer-key-here'
+consumer_secret = 'your-consumer-secret-here'
