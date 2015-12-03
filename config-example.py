3d5993d1b4840ad38bed41eb3ae8ccb843881b24
Christopher C Strelioff <chris.strelioff@gmail.com>
2015-12-02 17:10:34 -0800

    add config-example.py

---
+++ config-example.py
@@ -0,0 +1,21 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2015 Christopher C. Strelioff <chris.strelioff@gmail.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+config.py
+=========
+
+An example of the config.py file -- change this filename to config.py for
+using with exmple scripts. The gitignore file has been setup to ignore the 
+config.py fiel, keeping tokens safe.
+
+Keep all secretet tokens here -- set these up at this line:
+
+    https://www.discogs.com/settings/developers
+"""
+user_token = 'your-user-token-here'
